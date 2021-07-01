929fd3c383ba782a72a90d14c64dc828220bfabf
Mehedi Hasan <mhasan172036@mscse.uiu.ac.bd>
2021-07-01 19:02:24 +0600

    added

---
+++ lib/data/rest_data.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'package:ami_coding_pari_na/models/user.dart';
+import 'package:ami_coding_pari_na/utils/network_util.dart';
+
+class RestData {
+  NetworkUtil _netUtil = new NetworkUtil();
+  static final BASE_URL = "";
+  static final LOGIN_URL = BASE_URL + "/";
+
+  Future<User> login(String username, String password) {
+    return new Future.value(new User(username, password));
+  }
+}
